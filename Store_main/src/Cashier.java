55056d078de0c39c6e13124ae4f02ad39fae2a8d
Alexandervsz <alexander_5560@hotmail.com>
2021-04-24 17:26:22 +0200

    Finished cashier

--- Store_main/src/Cashier.java
+++ Store_main/src/Cashier.java
@@ -1,2 +1,24 @@
-public class Cashier {
+public class Cashier extends Employee {
+    private int register_number;
+    private double avg_cash_diff;
+
+    public int getRegister_number() {
+        return register_number;
+    }
+
+    public void setRegister_number(int register_number) {
+        this.register_number = register_number;
+    }
+
+    public double getAvg_cash_diff() {
+        return avg_cash_diff;
+    }
+
+    public void setAvg_cash_diff(double avg_cash_diff) {
+        this.avg_cash_diff = avg_cash_diff;
+    }
+
+    public Cashier(int employee_number, String name, double salary) {
+        super(employee_number, name, salary);
+    }
 }
