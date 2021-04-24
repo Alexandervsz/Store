876fe287f691b7869cd34031b67cca1fc00a370b
Alexandervsz <alexander_5560@hotmail.com>
2021-04-24 17:23:36 +0200

    Finished clerk

--- Store_main/src/Clerk.java
+++ Store_main/src/Clerk.java
@@ -1,2 +1,7 @@
-public class Clerk {
+public class Clerk extends Employee {
+    private double avg_stock_speed;
+
+    public Clerk(int employee_number, String name, double salary) {
+        super(employee_number, name, salary);
+    }
 }
