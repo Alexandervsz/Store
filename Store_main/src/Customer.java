38a529b4a3089ae5ceeaf7df81aa241866596a0c
Alexandervsz <alexander_5560@hotmail.com>
2021-04-24 17:46:02 +0200

    Finished customer

--- Store_main/src/Customer.java
+++ Store_main/src/Customer.java
@@ -1,2 +1,9 @@
+import java.time.LocalDateTime;
+
 public class Customer {
+    private String card_number;
+    private String name;
+    private String address;
+    private LocalDateTime last_seen;
+    private double total_spent;
 }
