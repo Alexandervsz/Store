437dc44a7d9fab328a335469cb089e4d8fea50ad
Alexandervsz <alexander_5560@hotmail.com>
2021-04-24 23:56:54 +0200

    Changed variable type

--- Store_main/src/Transaction.java
+++ Store_main/src/Transaction.java
@@ -2,14 +2,14 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Transaction {
-    private ArrayList<Product_Count> products;
+    private ArrayList<Product> products;
     private LocalDateTime date;
     private int cashier_number;
     private int register_number;
     private String customer_card_number;
     private String payment_method;
 
-    public Transaction(ArrayList<Product_Count> products, LocalDateTime date, int cashier_number, int register_number, String customer_card_number, String payment_method) {
+    public Transaction(ArrayList<Product> products, LocalDateTime date, int cashier_number, int register_number, String customer_card_number, String payment_method) {
         this.products = products;
         this.date = date;
         this.cashier_number = cashier_number;
@@ -18,11 +18,18 @@ public class Transaction {
         this.payment_method = payment_method;
     }
 
-    public Transaction(ArrayList<Product_Count> products, LocalDateTime date, int cashier_number, int register_number, String payment_method) {
+    public Transaction(ArrayList<Product> products, LocalDateTime date, int cashier_number, int register_number, String payment_method) {
         this.products = products;
         this.date = date;
         this.cashier_number = cashier_number;
         this.register_number = register_number;
         this.payment_method = payment_method;
+    }
+    public void AddItemsToInventory(Inventory inventory){
+        for (Product product: products){
+            inventory.addProduct(product);
+
+        }
+
     }
 }
