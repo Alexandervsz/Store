64aa97b59adbb97d9a485d359532f2c6eb33d801
Alexandervsz <alexander_5560@hotmail.com>
2021-04-24 17:46:22 +0200

    Added product_count class

---
+++ Store_main/src/Product_Count.java
@@ -0,0 +1,6 @@
+public class Product_Count extends Product{
+    private int amount;
+    public Product_Count(String productname, double price, String productcode, int amount) {
+        super(productname, price, productcode);
+    }
+}
