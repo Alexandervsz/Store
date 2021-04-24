6c017b7f0f8694d796da8d3b9ed943dcdd9041ee
Alexandervsz <alexander_5560@hotmail.com>
2021-04-24 18:03:04 +0200

    Updated to match product

--- Store_main/src/Product_Count.java
+++ Store_main/src/Product_Count.java
@@ -1,6 +1,8 @@
 public class Product_Count extends Product{
     private int amount;
-    public Product_Count(String productname, double price, String productcode, int amount) {
-        super(productname, price, productcode);
+
+    public Product_Count(String productname, double price, String productcode, int stock, int amount) {
+        super(productname, price, productcode, stock);
+        this.amount = amount;
     }
 }
