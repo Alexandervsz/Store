4534a28fdce712fd43cadbf53fa8422abfb54240
Alexandervsz <alexander_5560@hotmail.com>
2021-04-24 17:38:02 +0200

    Finished transaction

--- Store_main/src/Transaction.java
+++ Store_main/src/Transaction.java
@@ -1,2 +1,28 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Transaction {
+    private ArrayList<Product> products;
+    private LocalDateTime date;
+    private int cashier_number;
+    private int register_number;
+    private String customer_card_number;
+    private String payment_method;
+
+    public Transaction(ArrayList<Product> products, LocalDateTime date, int cashier_number, int register_number, String customer_card_number, String payment_method) {
+        this.products = products;
+        this.date = date;
+        this.cashier_number = cashier_number;
+        this.register_number = register_number;
+        this.customer_card_number = customer_card_number;
+        this.payment_method = payment_method;
+    }
+
+    public Transaction(ArrayList<Product> products, LocalDateTime date, int cashier_number, int register_number, String payment_method) {
+        this.products = products;
+        this.date = date;
+        this.cashier_number = cashier_number;
+        this.register_number = register_number;
+        this.payment_method = payment_method;
+    }
 }
