4fb1e5a43bd1d1d5510fd85f2cf729dac37d11ce
Alexandervsz <alexander_5560@hotmail.com>
2021-04-24 17:33:05 +0200

    Finished product

--- Store_main/src/Product.java
+++ Store_main/src/Product.java
@@ -1,2 +1,49 @@
 public class Product {
+    private String productname;
+    private double price;
+    private int stock;
+    private String productcode;
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getProductcode() {
+        return productcode;
+    }
+
+    public void setProductcode(String productcode) {
+        this.productcode = productcode;
+    }
+
+
+    public Product(String productname, double price, String productcode) {
+        this.productname = productname;
+        this.price = price;
+        this.productcode = productcode;
+    }
+
+    public void restock(int amount){
+        setStock(getStock()+amount);
+    }
 }
