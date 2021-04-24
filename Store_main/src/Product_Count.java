19afa2b596bfccc2beb10a0c972ea41c91b248aa
Alexandervsz <alexander_5560@hotmail.com>
2021-04-24 18:06:26 +0200

    Added getamount

--- Store_main/src/Product_Count.java
+++ Store_main/src/Product_Count.java
@@ -1,8 +1,12 @@
 public class Product_Count extends Product{
-    private int amount;
+    private final int amount;
 
     public Product_Count(String productname, double price, String productcode, int stock, int amount) {
         super(productname, price, productcode, stock);
         this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
     }
 }
