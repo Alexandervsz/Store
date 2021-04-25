7c6da63403cc2ff910b390c04d6e9fcf4f211fd2
Alexandervsz <alexander_5560@hotmail.com>
2021-04-25 12:49:31 +0200

    Bugfixes

--- Store_main/src/Main.java
+++ Store_main/src/Main.java
@@ -5,15 +5,14 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Inventory employeeInventory = new Inventory();
-        Employee employee = new Employee(1, "Bob", employeeInventory);
+        Employee employee = new Employee(1, "Bob");
         Customer customer;
-        employeeInventory.addProduct(new Product("H", 50.0, "H", 50));
-        employeeInventory.addProduct(new Product("O", 50.0, "H", 50));
+        employee.addProduct(new Product("H", 50.0, "H", 50));
+        employee.addProduct(new Product("O", 50.0, "H", 50));
         System.out.println("Welkom, wat is je naam?");
         Scanner scanner = new Scanner(System.in);
         String username = scanner.nextLine();
-        customer = new Customer(username, new Inventory());
+        customer = new Customer(username);
         PrintIntro(employee, customer);
 
 
@@ -38,18 +37,20 @@ public class Main {
 
     public static void OpenShop(Employee employee, Customer customer) {
         System.out.println(employee.toString());
+        System.out.println("Je hebt zelf nog "+customer.getMoney()+ " over.");
         boolean found = false;
         Product soldProduct = null;
         while (!found) {
             Scanner scanner = new Scanner(System.in);
             String userChoice = scanner.nextLine();
-            ArrayList<Product> products = employee.getInventory().getProducts();
+            ArrayList<Product> products = employee.getProducts();
             ArrayList<String> productnames = new ArrayList<>();
 
             for (Product product : products) {
                 if (userChoice.equals(product.getProductName())) {
                     found = true;
                     soldProduct = product;
+                    System.out.println(soldProduct.getAmount());
                     break;
                 }
             }
@@ -60,16 +61,31 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             int userChoice = scanner.nextInt();
             double money = customer.getMoney();
-            double totalprice = soldProduct.getAmount() * soldProduct.getPrice();
-            if (soldProduct.getAmount() >= userChoice & money > totalprice) {
-                employee.getInventory().updateProductStock(soldProduct, userChoice);
-                customer.getInventory().addProduct(soldProduct);
+            double totalprice = userChoice * soldProduct.getPrice();
+            if (employee.fetchProduct(soldProduct).getAmount() >= userChoice & money >= totalprice) {
+                employee.updateProductStock(soldProduct, userChoice);
+                Product fetchedProduct = employee.fetchProduct(soldProduct);
+                if (fetchedProduct.getAmount() == 0){
+                    employee.removeProduct(fetchedProduct);
+                }
+                customer.addProduct(soldProduct);
                 customer.setMoney(money - totalprice);
                 System.out.println("Bedankt!");
                 sold = true;
 
-            } else {
-                System.out.println("Helaas, de verkoop wordt gestaakt.");
+            }
+            else {
+                if(money < totalprice){
+                    System.out.println(money);
+                    System.out.println(totalprice);
+                    System.out.println("Je hebt te weinig geld.");
+
+                }
+                else if(employee.fetchProduct(soldProduct).getAmount() < userChoice){
+                    System.out.println("Ik heb er maar "+employee.fetchProduct(soldProduct).getAmount()+"\n");
+                }
+
+                OpenShop(employee, customer);
             }
 
         }
