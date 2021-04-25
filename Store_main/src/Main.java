de8bee5069b228f420a7acd789d999eefbd95b17
Alexandervsz <alexander_5560@hotmail.com>
2021-04-25 16:56:03 +0200

    bugfixes.

--- Store_main/src/Main.java
+++ Store_main/src/Main.java
@@ -1,23 +1,20 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
         Employee employee = new Employee();
-        Customer customer;
         employee.addProduct(new Product("H", 50.0, "H", 50));
         employee.addProduct(new Product("O", 50.0, "O", 100));
         employee.addProduct(new Product("C", 500.0, "C", 50));
-        customer = new Customer();
-        PrintIntro(employee, customer);
-
-
+        Customer customer = new Customer();
+        printIntro(employee, customer);
     }
 
-
-    public static void PrintIntro(Employee employee, Customer customer) {
+    public static void printIntro(Employee employee, Customer customer) {
         System.out.println("Wat wil je doen?");
         System.out.println("Dingen kopen (1):");
         System.out.println("Dingen combineren (2):");
@@ -25,44 +22,25 @@ public class Main {
         Scanner scanner2 = new Scanner(System.in);
         int userChoice = scanner2.nextInt();
         switch (userChoice) {
-            case 1 -> OpenShop(employee, customer);
-            case 2 -> CombineItems(employee, customer);
-            case 3 -> SellItems(employee, customer);
-            default -> PrintIntro(employee, customer);
+            case 1 -> openShop(employee, customer);
+            case 2 -> combineItems(employee, customer);
+            case 3 -> sellItems(employee, customer);
+            default -> printIntro(employee, customer);
         }
-
-
     }
 
-
-    public static void OpenShop(Employee employee, Customer customer) {
+    public static void openShop(Employee employee, Customer customer) {
         System.out.println(employee.toString());
         System.out.println("Je hebt zelf nog " + customer.getMoney() + " over.");
-        boolean found = false;
-        Product soldProduct = null;
-        while (!found) {
-            System.out.println("Voer hier je input in: ");
-            Scanner scanner = new Scanner(System.in);
-            String userChoice = scanner.nextLine();
-            ArrayList<Product> products = employee.getProducts();
-
-            for (Product product : products) {
-                if (userChoice.equals(product.getProductName())) {
-                    found = true;
-                    soldProduct = product;
-                    break;
-                }
-            }
-        }
+        Product soldProduct = findProduct(employee);
         System.out.println("Hoeveel wil je er kopen?");
         boolean sold = false;
         while (!sold) {
-            Scanner scanner = new Scanner(System.in);
-            int userChoice = scanner.nextInt();
+            int userChoice = findInt(employee, soldProduct);
             double money = customer.getMoney();
             double totalprice = userChoice * soldProduct.getPrice();
 
-            if (employee.fetchProduct(soldProduct).getAmount() >= userChoice & money >= totalprice) {
+            if (employee.fetchProduct(soldProduct).getAmount() >= userChoice && money >= totalprice && userChoice >= 0) {
                 employee.updateProductStock(soldProduct, userChoice * -1);
 
                 if (soldProduct.getAmount() == 0) {
@@ -73,13 +51,15 @@ public class Main {
                     Product oldProduct = customer.fetchProduct(soldProduct);
                     customer.updateProductStock(oldProduct, userChoice);
                 } else {
-                    Product newProduct = new Product(soldProduct.getProductName(), soldProduct.getPrice(), soldProduct.getProductcode(), userChoice);
-                    customer.addProduct(newProduct);
+                    if (userChoice != 0) {
+                        Product newProduct = new Product(soldProduct.getProductName(), soldProduct.getPrice(), soldProduct.getProductCode(), userChoice);
+                        customer.addProduct(newProduct);
+                    }
                 }
                 customer.setMoney(money - totalprice);
                 System.out.println("Bedankt!");
                 sold = true;
-                PrintIntro(employee, customer);
+                printIntro(employee, customer);
 
             } else {
                 if (money < totalprice) {
@@ -89,31 +69,33 @@ public class Main {
 
                 } else if (employee.fetchProduct(soldProduct).getAmount() < userChoice) {
                     System.out.println("Ik heb er maar " + employee.fetchProduct(soldProduct).getAmount() + "\n");
+                } else if (userChoice < 0) {
+                    System.out.println("ongeldige invoer, verkoop gestaakt.");
                 }
-
-                OpenShop(employee, customer);
+                openShop(employee, customer);
             }
-
         }
-
-
     }
 
-    public static void CombineItems(Employee employee, Customer customer) {
+    public static void combineItems(Employee employee, Customer customer) {
 
-        if (getCustomerItems(customer)) {
+        if (getCustomerItems(customer, 1)) {
             System.out.println("Kies je eerste product.");
-            Product product1 = FindProduct(customer);
+            Product product1 = findProduct(customer);
             System.out.println("Kies je hoeveelheid eerste product.");
-            int amount1 = FindInt(customer, product1);
+            int amount1 = findInt(customer, product1);
             System.out.println("Kies je tweede product.");
-            Product product2 = FindProduct(customer);
+            Product product2 = findProduct(customer);
+            if (product1.equals(product2)) {
+                System.out.println("Twee keer hetzelfde product gekozen!");
+                combineItems(employee, customer);
+            }
             System.out.println("Kies je hoeveelheid tweede product.");
-            int amount2 = FindInt(customer, product2);
+            int amount2 = findInt(customer, product2);
             CombineItem itemCombiner = new CombineItem(product1, product2, amount1, amount2);
             Product newProduct = itemCombiner.addItems();
             if (newProduct != null) {
-                System.out.println("Je hebt " + newProduct.getProductName() + " gemaakt!");
+                System.out.println("Je hebt " + newProduct.getProductName() + " gemaakt!\n");
                 customer.updateProductStock(product1, amount1 * -1);
                 customer.updateProductStock(product2, amount2 * -1);
                 if (customer.checkInventory(newProduct)) {
@@ -123,29 +105,28 @@ public class Main {
                     customer.addProduct(newProduct);
                 }
             } else {
-                System.out.println("Tot je teleurstelling gebeurt er niets.");
+                System.out.println("Tot je teleurstelling gebeurt er niets.\n");
 
             }
-            PrintIntro(employee, customer);
+            printIntro(employee, customer);
         } else {
-            PrintIntro(employee, customer);
+            printIntro(employee, customer);
         }
-
-
     }
 
-
-    public static Product FindProduct(Customer customer) {
+    public static Product findProduct(Inventory customer) {
         Product foundProduct = null;
         boolean found = false;
         while (!found) {
             System.out.println("Voer hier je input in: ");
             Scanner scanner = new Scanner(System.in);
             String userChoice = scanner.nextLine();
+
+
             ArrayList<Product> products = customer.getProducts();
 
             for (Product product : products) {
-                if (userChoice.equals(product.getProductName())) {
+                if (userChoice.equals(product.getProductName()) || userChoice.toUpperCase(Locale.ROOT).equals(product.getProductName())) {
                     found = true;
                     foundProduct = product;
                     break;
@@ -155,65 +136,70 @@ public class Main {
         return foundProduct;
     }
 
-    public static int FindInt(Customer customer, Product product) {
+    public static int findInt(Inventory customer, Product product) {
         System.out.println("Voer hier je input in: ");
         Scanner scanner = new Scanner(System.in);
-        int userChoice = scanner.nextInt();
-        if (userChoice <= customer.fetchProduct(product).getAmount()) {
-            return userChoice;
-        } else {
-            System.out.println("Niet voldoende product.");
-            FindInt(customer, product);
+        int userChoice = -1;
+        try {
+            userChoice = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Ongeldige invoer!");
+            findInt(customer, product);
         }
-        return -1;
-
+        if (userChoice <= customer.fetchProduct(product).getAmount() && userChoice >= 0) {
+            return userChoice;
+        } else if (userChoice < 0) {
+            System.out.println("Ongeldige invoer!");
+            findInt(customer, product);
+        } else if (userChoice <= customer.fetchProduct(product).getAmount()) {
+            System.out.println("Niet voldoende product.");
+            findInt(customer, product);
+        }
+        return userChoice;
     }
 
-    public static boolean getCustomerItems(Customer customer) {
+    public static boolean getCustomerItems(Customer customer, int min) {
         System.out.print("Je hebt ");
-        if ((customer.getProducts().size()) > 0) {
+        if ((customer.getProducts().size()) > min) {
             System.out.println("\n");
         } else {
-            System.out.println("niets.\n");
+            System.out.println("niet voldoende producten.\n");
             return false;
         }
         StringBuilder string = new StringBuilder();
-
         for (Product product : customer.getProducts()) {
             string.append(product.getProductName());
             string.append(", ");
             string.append(product.getAmount());
             string.append(" stuks.");
             string.append("\n");
-
-
         }
-
         System.out.println(string);
         return true;
-
     }
 
-    public static void SellItems(Employee employee, Customer customer) {
-        if (getCustomerItems(customer)) {
+    public static void sellItems(Employee employee, Customer customer) {
+        if (getCustomerItems(customer, 0)) {
             System.out.println("Welk product wil je verkopen?");
-            Product product1 = FindProduct(customer);
-            System.out.println("Hoe veel wil je er verkopen?");
-            int amount1 = FindInt(customer, product1);
+            Product product1 = findProduct(customer);
+            System.out.println("Hoeveel wil je er verkopen?");
+            int amount1 = findInt(customer, product1);
+            if (amount1 < 0) {
+                printIntro(employee, customer);
+                return;
+            }
             if (employee.checkInventory(product1)) {
                 Product oldProduct = employee.fetchProduct(product1);
                 employee.updateProductStock(oldProduct, amount1);
             } else {
-                Product employeeProduct = new Product(product1.getProductName(), product1.getPrice(), product1.getProductcode(), amount1);
+                Product employeeProduct = new Product(product1.getProductName(), product1.getPrice(), product1.getProductCode(), amount1);
                 employee.addProduct(employeeProduct);
             }
             customer.updateProductStock(product1, amount1 * -1);
             customer.setMoney(customer.getMoney() + product1.getPrice() * amount1);
             System.out.println("Bedankt!");
         }
-        PrintIntro(employee, customer);
-
-
+        printIntro(employee, customer);
     }
 }
 
